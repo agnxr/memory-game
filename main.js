19ebedf42804f7cafd1c385371e1faf0511bc3be
Agnieszka Rakowska <ar21@o2.pl>
2019-10-08 17:11:33 +0200

    code refactoring

--- main.js
+++ main.js
@@ -1,36 +1,27 @@
-var backgrounds = ["watermelon.svg", "banana.svg", "lime.svg", "lime.svg", "grapes.svg", "mangosteen.svg",  "banana.svg", "passion-fruit.svg", "watermelon.svg", "mangosteen.svg", "passion-fruit.svg", "grapes.svg"];
-//losowanie ale zeby liczba par sie zgadzala
-// mozliwosc zresetowania planszy
-// czy kazda karta powinna byc osobnym elemenetem jquery - opakowac wszystkie karty w 1 obiekt
-//mozna z tego zrobic gre w sapera albo kolo fortuny
-var randomBg = backgrounds.sort(function(a, b){return 0.5 - Math.random()});
+const backgrounds = ["watermelon.svg", "banana.svg", "lime.svg", "lime.svg", "grapes.svg", "mangosteen.svg",  "banana.svg", "passion-fruit.svg", "watermelon.svg", "mangosteen.svg", "passion-fruit.svg", "grapes.svg"];
+const randomBg = backgrounds.sort(function(a, b){return 0.5 - Math.random()});
+const cards = document.querySelectorAll(".card");
 
-var cards = document.querySelectorAll(".card");
-var oneVisible = false;
-var turnCounter = 0; //licznik rund
-var visibleIndex; //index karty aktualnie widocznej to compare if both bakcgorunds are the same
-var lock = false;
-var pairsLeft = 6;
-
+let oneVisible = false;
+let turnCounter = 0; //licznik rund
+let visibleIndex; //index of visible card to compare if both backgrounds are the same
+let lock = false;
+let pairsLeft = 6;
 
 cards.forEach(function(item, index) {
     item.addEventListener("click", function(){ 
-        //alert(("ma index: " + index));
         if (!lock) {
             lock = true;
             var bg = `url("images/${randomBg[index]}")`;
-            //var bg = `url("images/lime.svg")`;
-            $(`#c${index}`).css('background-image', bg);
-            //$(`#c${index}`).css('background-size', 'contain');
-            $(`#c${index}`).addClass('active');
-            //$(`#c${index}`).removeClass('card');
+            $(item).css('background-image', bg);
+            $(item).addClass('active');
 
             if (!oneVisible) {
                 oneVisible = true;
                 visibleIndex = index;
                 lock = false
             } else {
-                backgrounds[visibleIndex] == backgrounds[index] ? setTimeout(function() {hide2cards(index, visibleIndex)}, 750) : setTimeout(function() {restore2cards(index, visibleIndex)}, 1000);
+                backgrounds[visibleIndex] == backgrounds[index] ? setTimeout(function() { hide2cards(index, visibleIndex) }, 750) : setTimeout(function() {restore2cards(index, visibleIndex)}, 1000);
 
                 turnCounter++;
                 $('.score').html(`Turn counter: ${turnCounter}`);
@@ -46,7 +37,7 @@ function hide2cards(firstItem, secondItem) {
     $(`#c${secondItem}`).css('visibility', 'hidden');
     pairsLeft--;
     if(pairsLeft === 0){
-        $('.board').html(`<h1>You win!<br>Done in ${turnCounter} turns </h1>`);
+        $('.board').html(`<h1>You win!<br> Done in ${turnCounter} turns </h1>`);
     }
     lock = false;
 }
@@ -56,7 +47,6 @@ function restore2cards(firstItem, secondItem) {
     $(`#c${firstItem}`).removeClass('active');
     $(`#c${secondItem}`).css('background-image', 'url("images/question.svg")');
     $(`#c${secondItem}`).removeClass('active');
-
     lock = false;
 }
 
